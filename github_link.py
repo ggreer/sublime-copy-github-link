e32ff06c86805a2a1ef52c3c45b1680b7067be94
Geoff Greer <geoff@greer.fm>
2020-04-02 01:00:57 -0700

    Cleanup. Now this works with line numbers.

--- github_link.py
+++ github_link.py
@@ -1,6 +1,5 @@
 import sublime, sublime_plugin
 
-from urllib.parse import urlparse
 import os
 import re
 import subprocess
@@ -26,14 +25,13 @@ class CopyGithubLinkCommand(sublime_plugin.TextCommand):
     def get_repo_url(self):
         filename = self.view.file_name()
         if not filename or len(filename) == 0:
-            sublime.status_message('Can\'t copy: No filename for view.')
             return
         remote = self.run_git(['git', 'config', '--get', 'remote.origin.url'], os.path.dirname(filename))
         if not remote:
             return
         if remote[:4] == 'git@':
             # ssh remote. transform to https
-            p = re.compile('^git@(?P<host>[^:]+):(?P<path>.*)\.git$')
+            p = re.compile('^git@(?P<host>[^:]+):(?P<path>.*)$')
             m = p.match(remote)
             if not m:
                 print('Unable to parse remote url %s' % remote)
@@ -42,8 +40,8 @@ class CopyGithubLinkCommand(sublime_plugin.TextCommand):
             path = m.group('path')
             remote = 'https://%s/%s' % (host, path)
         print('parsed remote %s' % remote)
-        u = urlparse(remote)
-
+        if remote[-4:] == '.git':
+            remote = remote[:-4]
         return remote
   
 
@@ -57,15 +55,30 @@ class CopyGithubLinkCommand(sublime_plugin.TextCommand):
         branchname = self.run_git(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], dirname)
         project_dir = self.run_git(['git', 'rev-parse', '--show-toplevel'], dirname)
         relpath = self.run_git(['git', 'ls-files', '--error-unmatch', filename], project_dir)
+        if not relpath:
+            sublime.status_message("File is not tracked in git.")
+            return
         repo_url = self.get_repo_url()
         if not repo_url:
             sublime.status_message("Error: No remote url for project")
             return
         url = '%s/blob/%s/%s' % (repo_url, branchname, relpath) # todo: line number
+        regions = self.view.sel()
+        if len(regions) > 0:
+            line, col = self.view.rowcol(regions[0].begin())
+            print('LINE', line)
+            url += '#L%s' % (line + 1)
         print('url: %s' % url)
         sublime.set_clipboard(url)
         sublime.status_message("Copied Github link")
 
 
     def is_enabled(self):
-        return bool(self.get_repo_url())
+        if not self.get_repo_url():
+            return false
+
+        filename = self.view.file_name()
+        dirname = os.path.dirname(filename)
+        project_dir = self.run_git(['git', 'rev-parse', '--show-toplevel'], dirname)
+        relpath = self.run_git(['git', 'ls-files', '--error-unmatch', filename], project_dir)
+        return bool(relpath)
